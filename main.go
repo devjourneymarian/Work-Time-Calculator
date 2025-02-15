e8c3901a5d7cdca70f986fdc22b55ad9ea188c57
marian16888 <114952885+marian16888@users.noreply.github.com>
2025-02-16 00:49:06 +0100

    WIP

--- main.go
+++ main.go
@@ -6,6 +6,32 @@ import (
 )
 
 func main() {
-	time := time.Now()
-	fmt.Println(time)
+	var choice int
+	fmt.Println("Welcome to the Work-Time-Caluclator! Please choose an option:")
+	options()
+	fmt.Scanln(&choice)
+	if choice == 1 {
+		clockIn()
+	}
+}
+
+func options() {
+	fmt.Println("1. Simple clock-in calculator")
+}
+
+func clockIn() {
+	var ClockInTime string
+
+	fmt.Println("Enter the time you clocked in: HH:MM")
+	fmt.Scanln(&ClockInTime)
+
+	parsedClockinTime, err := time.Parse("15:04", ClockInTime)
+	if err != nil {
+		fmt.Println("Error parsing time")
+		return
+	}
+
+	fmt.Println("You clocked in at: ", parsedClockinTime.Format("15:04"))
+
+	//to-do: add working hours and add onto the clock in time
 }
