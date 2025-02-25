f98bd2c5422edbc13b324356bc3dbedfa4f2a5cd
marian16888 <114952885+marian16888@users.noreply.github.com>
2025-02-25 18:25:40 +0100

    last commit
    Deciding to halt the project. Go isn't as enjoyable as I thought. I want to pursue more fun projects.

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	var choice int
-	fmt.Println("Welcome to the Work-Time-Caluclator! Please choose an option:")
+	fmt.Println("Welcome to the Work-Time-Calculator! \nPlease choose an option by entering one of the following numbers:")
 	options()
 	fmt.Scanln(&choice)
 	if choice == 1 {
@@ -21,6 +21,7 @@ func options() {
 
 func clockIn() {
 	var ClockInTime string
+	var StayTime string
 
 	fmt.Println("Enter the time you clocked in: HH:MM")
 	fmt.Scanln(&ClockInTime)
@@ -33,5 +34,30 @@ func clockIn() {
 
 	fmt.Println("You clocked in at: ", parsedClockinTime.Format("15:04"))
 
-	//to-do: add working hours and add onto the clock in time
+	fmt.Println("Enter how long you want to stay in HH:MM format")
+	fmt.Scanln(&StayTime)
+
+	workDuration, err := parseDuration(StayTime)
+	if err != nil {
+		fmt.Println("Invalid duration format! Use HH:MM.")
+		return
+	}
+
+	parsedStayTime := parsedClockinTime.Add(workDuration)
+
+	fmt.Println("You have to work until: ", parsedStayTime.Format("15:04"))
+}
+
+func parseDuration(input string) (time.Duration, error) {
+	var hours, minutes int
+	parsedValuesCount, err := fmt.Sscanf(input, "%d:%d", &hours, &minutes)
+	if err != nil {
+		fmt.Printf("Error parsing duration: %v\n", err)
+		return 0, err
+	}
+	if parsedValuesCount != 2 {
+		fmt.Println("Invalid duration format! Use HH:MM.")
+		return 0, fmt.Errorf("invalid duration format")
+	}
+	return time.Duration(hours)*time.Hour + time.Duration(minutes)*time.Minute, nil
 }
